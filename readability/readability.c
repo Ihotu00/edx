23ec2375f8ab148d0788e5be1aa9ff85f2e64396
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-06-16 21:08:22 +0000

    Fri, Jun 16, 2023, 10:08 PM +01:00

--- readability/readability.c
+++ readability/readability.c
@@ -39,6 +39,8 @@ int count_words(string text)
     {
         if isspace(text[i])
         {
+            for (int j = 0; j < l; j++)
+            if isalpha(text[i + 1])
             word++;
         }
     }

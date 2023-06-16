7ecda2d4a2253d3a61c3e848d6f23a17e4d7288e
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-06-16 23:41:56 +0000

    Sat, Jun 17, 2023, 12:41 AM +01:00

--- readability/readability.c
+++ readability/readability.c
@@ -17,7 +17,7 @@ int main(void)
     int sentence = count_sentence(text);
     int grade = (0.0588 * (letters / word) * 100) - (0.296 * (sentence / word) * 100) - 15.8;
     grade = round(grade);
-    if (grade > 1 && grade < 16)
+    if (grade >= 1 && grade <= 16)
     {
         printf("Grade %i\n", grade);
     }

4693bfea48ed15153e004647562f85b38be577ca
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-06-16 18:31:43 +0000

    Fri, Jun 16, 2023, 7:31 PM +01:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,3 +1,4 @@
+# dictionary of all the fruits and their calories
 fruits = {
     "apple": "130", "avocado": "50", "banana": "110", "cantaloupe": "50",
     "grapefruit": "60", "grapes": "90", "honeydew": "50", "melon": "50",
@@ -7,7 +8,9 @@ fruits = {
     "tangerine": "50", "watermelon": "80",
 }
 
+# accept user input
 item = input("Item: ").lower()
 
+# find input in the dict and print out the corresponding calories
 if item in fruits:
     print("Calories: ", fruits[item])

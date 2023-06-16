b421c9b5dd3410299174360dfe032df71d41ef96
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-06-16 18:19:31 +0000

    Fri, Jun 16, 2023, 7:19 PM +01:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,41 +1,11 @@
 fruits = {
-    130
-Avocado
-	50
-Banana
-	110
-Cantaloupe
-	50
-Grapefruit
-	60	
-Grapes
-	90
-Honeydew
-Melon
-1/10 medium
-melon
-	50
-Kiwifruit
-	90
-Lemon
-	15
-Lime
-	20
-Nectarine
-	60
-Orange
-	80
-Peach
-	60
-Pear
-	100
-Pineapple
-	50
-Plums	70
-Strawberries	50
-Sweet
-Cherries
-21 cherries;	100
-Tangerine	50
-Watermelon	80
+    "Apple": "130", "Avocado": "50", "Banana": "110", "Cantaloupe": "50",
+    "Grapefruit": "60", "Grapes": "90", "Honeydew": "50", "Melon": "50",
+    "Kiwifruit": "90", "Lemon": "15", "Lime": "20", "Nectarine": "60",
+    "Orange": "80", "Peach": "60", "Pear": "100", "Pineapple": "50",
+    "Plums": "70", "Strawberries": "50", "Sweet Cherries": "100",
+    "Tangerine": "50", "Watermelon": "80",
 }
+
+for fruit in fruits:
+    print(fruit)

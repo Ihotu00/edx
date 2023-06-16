011e05a84109484b6aba3e0fc146e741ea2689ec
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-06-16 22:28:40 +0000

    Fri, Jun 16, 2023, 11:28 PM +01:00

--- readability/readability.c
+++ readability/readability.c
@@ -15,7 +15,7 @@ int main(void)
     int word = count_words(text);
     int sentence = count_sentence(text);
     int grade = (letters / word * 100) - (sentence / word * 100) - 15.8;
-    printf("%iletters\n%iwords\n%isentences\n%iGrade\n", letters, word, sentence, grade);
+    printf("%i letters\n%i words\n%i sentences\n%i Grade\n", letters, word, sentence, grade);
 }
 
 int count_letters(string text)

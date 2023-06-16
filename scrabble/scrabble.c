4eb2971a12e2fad5383156f25cd9ba0a9d5e7de1
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-06-16 19:19:53 +0000

    Fri, Jun 16, 2023, 8:19 PM +01:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -2,7 +2,6 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
-#include <conio.h>
 
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
@@ -37,14 +36,18 @@ int main(void)
 int compute_score(string word)
 {
     // convert word to lowercase
-    word = strlwr(word);
+    // word = strlwr(word);
 
     // take the lenght of the word
-    len = strlen(word);
+    int len = strlen(word);
 
     int score = 0;
-    for (int i = o, i < len; i++)
+    for (int i = 0; i < len; i++)
     {
+        if(isupper(word[i]))
+        {
+            word[i] = tolower(word[i]);
+        }
         score += POINTS[word[i] - "a"];
     }
     return score;

65c81fb2ea9fcacd9926522489ff3a8476a8db55
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-06-16 19:49:22 +0000

    Fri, Jun 16, 2023, 8:49 PM +01:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -43,11 +43,7 @@ int compute_score(string word)
     for (int i = 0; i < len; i++)
     {
         word[i] = tolower(word[i]);
-        // if(isupper(word[i]))
-        // {
-        //     // convert character to lowercase
-        //     word[i] = tolower(word[i]);
-        // }
+        if word[i] in [',! "?.]
         score += POINTS[word[i] - 'a'];
     }
     return score;

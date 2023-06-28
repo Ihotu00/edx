97f3b7f59fa4ca50000466a8deac2b73d1f31d2c
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-06-28 20:29:40 +0000

    Wed, Jun 28, 2023, 9:29 PM +01:00

--- game/game.py
+++ game/game.py
@@ -1,11 +1,12 @@
 from random import randint
 
-while True:
-    try:
-        level = int(input("Level: "))
-        break
-    except ValueError:
-        pass
+def check(s)
+    while True:
+        try:
+            s = int(input("Level: "))
+            break
+        except ValueError:
+            pass
 while True:
     try:
         guess = int(input("Guess: "))

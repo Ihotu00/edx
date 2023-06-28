ee60d1cf65b4e0657a9d739d606ca02260b8d7e0
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-06-28 20:21:38 +0000

    Wed, Jun 28, 2023, 9:21 PM +01:00

--- game/game.py
+++ game/game.py
@@ -1,8 +1,11 @@
 from random import randint
 
-level = int(input("Level: "))
 number = randint(1, level)
 while True:
+    try:
+        level = int(input("Level: "))
+    except ValueError:
+        pass
     guess = int(input("Guess: "))
     if guess < number:
         print("Too small!")

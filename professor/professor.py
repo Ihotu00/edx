d9dc3d9bd817e59b079a59cf612993703363f88e
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-06-28 21:36:34 +0000

    Wed, Jun 28, 2023, 10:36 PM +01:00

--- professor/professor.py
+++ professor/professor.py
@@ -2,5 +2,5 @@ import random
 
 # def get_level():
 level = 0
-while (level != 1 or level != 2 or level != 3):
-    level = input("Level: ")
+while (level < 1 or level > 3):
+    level = int(input("Level: "))

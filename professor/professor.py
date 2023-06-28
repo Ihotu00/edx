c7b0328e4c82d94e3bfb3f508e50492c39a788cb
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-06-28 21:55:43 +0000

    Wed, Jun 28, 2023, 10:55 PM +01:00

--- professor/professor.py
+++ professor/professor.py
@@ -1,5 +1,9 @@
 from random import randint
 
+def main():
+    l = get_level
+    print(generate_integer(l))
+
 def get_level():
     level = 0
     while (level < 1 or level > 3):
@@ -7,4 +11,16 @@ def get_level():
     return level
 
 def generate_integer(level):
-    x = randint
+    if level = 1:
+        x = randint(1, 9)
+        y = randint(1, 9)
+    elif level = 2:
+        x = randint(10, 99)
+        y = randint(10, 99)
+    else:
+        x = randint(100, 999)
+        y = randint(100, 999)
+    return x, y
+
+
+main()

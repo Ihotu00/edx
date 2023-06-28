27a58ee8deb9a86928cbe485c8e6854dc5d4a2d0
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-06-28 21:38:40 +0000

    Wed, Jun 28, 2023, 10:38 PM +01:00

--- professor/professor.py
+++ professor/professor.py
@@ -1,6 +1,9 @@
-import random
+from random import randint
 
-# def get_level():
-level = 0
-while (level < 1 or level > 3):
-    level = int(input("Level: "))
+def get_level():
+    level = 0
+    while (level < 1 or level > 3):
+        level = int(input("Level: "))
+
+def generate_integer(level):
+    x = randint

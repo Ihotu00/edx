1a8b668e92cc08813dc51bddae21dfec38bdd584
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-06-28 20:17:22 +0000

    Wed, Jun 28, 2023, 9:17 PM +01:00

--- game/game.py
+++ game/game.py
@@ -1,5 +1,11 @@
-import random
+from random import randint
 
 level = input("Level: ")
 guess = input("Guess: ")
 number = randint(1, level)
+if guess < number:
+    print("Too small!")
+elif guess > number:
+    print("Too large!")
+else:
+    print("Just right!")

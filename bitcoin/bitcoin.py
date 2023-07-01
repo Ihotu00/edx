d26698e884583ff672a47a3604363913863efa35
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-07-01 15:38:02 +0000

    Sat, Jul 1, 2023, 4:38 PM +01:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -10,7 +10,7 @@ if len(sys.argv) == 2:
         sys.exit()
 
 response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-sys
+sys.argv[1] = float(sys.argv[1])
 bpi = response.json()["bpi"]
 usd = bpi["USD"]
 amount = sys.argv[1] * usd["rate_float"]

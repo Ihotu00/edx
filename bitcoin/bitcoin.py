73f6bce10563d4862ad450b6a2639fb281acc5f3
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-07-01 15:45:03 +0000

    Sat, Jul 1, 2023, 4:45 PM +01:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -2,16 +2,15 @@ import requests
 import sys
 import json
 
-if len(sys.argv) == 2:
-    try:
-        float(sys.argv[1])
-        # print("OK")
-    except ValueError:
-        sys.exit()
+if len(sys.argv) != 2:
+    sys.exit()
+try:
+    sys.argv[1] = float(sys.argv[1])
+except ValueError:
+    sys.exit()
 
 response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-sys.argv[1] = float(sys.argv[1])
 bpi = response.json()["bpi"]
 usd = bpi["USD"]
 amount = float(sys.argv[1] * usd["rate_float"])
-print(f"${amount:, .4f}")
+print(f"${amount:,.4f}")

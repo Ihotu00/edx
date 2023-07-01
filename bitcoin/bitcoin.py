ab154f7468078eb5e2d422d96aadb68c2af9d8f9
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-07-01 15:28:24 +0000

    Sat, Jul 1, 2023, 4:28 PM +01:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -1,11 +1,13 @@
 import requests
 import sys
+import json
 
 if len(sys.argv) == 2:
     try:
         float(sys.argv[1])
-        print("OK")
+        # print("OK")
     except ValueError:
         sys.exit()
 
-re
+response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+print(json.dumps(response.json(), indent = 2))

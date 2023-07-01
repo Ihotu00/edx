5a185ccd3d567dee7bc074f2f3c99ab8b569f82a
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-07-01 15:49:12 +0000

    Sat, Jul 1, 2023, 4:49 PM +01:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -3,10 +3,12 @@ import sys
 import json
 
 if len(sys.argv) != 2:
+    print("Missing command-line argument")
     sys.exit()
 try:
     sys.argv[1] = float(sys.argv[1])
 except ValueError:
+    print("Command-line argument is not a number")
     sys.exit()
 
 response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")

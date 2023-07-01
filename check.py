742d48404de2dc6a5be6aa19d512c273241618e9
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-07-01 14:01:31 +0000

    Sat, Jul 1, 2023, 3:01 PM +01:00

--- check.py
+++ check.py
@@ -6,7 +6,7 @@ def main():
 def check(s):
     while True:
         try:
-            t = int(input(f"{s}: "))
+            t = int(input(f"{s=}: "))
             return True
         except ValueError:
             pass

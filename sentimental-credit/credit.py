589547372615e23d91230253167f63408db6c7e2
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-07-24 15:14:18 +0000

    Mon, Jul 24, 2023, 4:14 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -24,17 +24,17 @@ def pattern(card):
 def check_sum(card):
     sum = 0
     for i in reversed(range(1, len(card), 2)):
-        digit = int(card[i])
-        digit *= 2
-        sum += digit
+        # digit = int(card[i])
+        # digit *= 2
+        # sum += digit
     # for i in reversed(range(0, len(card), 2)):
     #     digit = int(card[i])
     #     sum += digit
     # return sum
-    print(sum)
+        print(card[i])
 
 
 
 if __name__ == "__main__":
     main()
-# amex:378282246310005, visa:4111111111111111, master: 5555555555554444
+# amex:378282246310005, visa:4111111111111111, 4003600000000014 master: 5555555555554444

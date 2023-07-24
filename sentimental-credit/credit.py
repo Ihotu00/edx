30df59335ff26f8d74c18efe981c1e316e61d19b
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-07-24 16:26:21 +0000

    Mon, Jul 24, 2023, 5:26 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,12 +1,15 @@
 import re
 
 # TODO
+
+
 def main():
     card = input("card: ")
     if check_sum(card) == 0:
         pattern(card)
     else:
         print("INVALID")
+
 
 def pattern(card):
 
@@ -27,20 +30,20 @@ def pattern(card):
     else:
         print("INVALID")
 
+
 def check_sum(card):
     sum = 0
     temp = card[:len(card) - 1]  # store input in variable without last digit
 
-    for i in temp[ : :-2]:  # first half of the check_sum: iterate through every other digit
+    for i in temp[::-2]:  # first half of the check_sum: iterate through every other digit
         digit = int(i)      # and multiply by 2
         digit *= 2
         for i in str(digit):
             sum += int(i)   # add each digit to sum
-    for i in card[ : :-2]:  # second half of check_sum: iterate through the other digits
+    for i in card[::-2]:  # second half of check_sum: iterate through the other digits
         digit = int(i)
         sum += digit
     return sum % 10     # return last digit of sum
-
 
 
 if __name__ == "__main__":

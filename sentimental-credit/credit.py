799e4925efbe905fdad9358f163e1fd9bea57021
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-07-24 15:25:56 +0000

    Mon, Jul 24, 2023, 4:25 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -26,12 +26,13 @@ def check_sum(card):
     for i in reversed(range(0, len(card), 2)):
         digit = int(card[i])
         digit *= 2
-        # sum += digit
-    # for i in reversed(range(1, len(card), 2)):
-    #     digit = int(card[i])
-    #     sum += digit
-    # return sum
-        print(digit)
+        for i in range(len)
+        sum += digit
+    for i in reversed(range(1, len(card), 2)):
+        digit = int(card[i])
+        sum += digit
+    return sum
+        # print(digit)
 
 
 

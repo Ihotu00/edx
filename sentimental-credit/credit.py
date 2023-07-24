8f98133ecbc6e8970731a1ce3177808c0b3b653b
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-07-24 13:57:49 +0000

    Mon, Jul 24, 2023, 2:57 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -2,10 +2,13 @@ import re
 
 # TODO
 amex = "^(34|37)\d{13}"
-visa = "^4\d{13}"
+visa = "^4\d{12, 15}"
 card = input("card: ")
-match = re.match(visa, card)
+match = re.match(amex, card)
 if match:
     print("AMEX")
 else:
     print("wrong")
+
+
+# amex:378282246310005, visa:4111111111111111

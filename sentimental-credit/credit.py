fabf489e52590e8f958c461aef4e8e3845d15af6
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-07-24 14:55:39 +0000

    Mon, Jul 24, 2023, 3:55 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -23,7 +23,7 @@ def pattern(card):
 
 def check_sum(card):
     sum = 0
-    for i in range(-2, len(card), -2):
+    for i in range(len(card), -2):
     #     card[i] *= 2
     #     sum += card[i]
     # for i in range(-1, len(card), -2):

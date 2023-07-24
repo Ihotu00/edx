b286e8537c175d21ddce7f38666e3e09aff7a109
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-07-24 14:34:31 +0000

    Mon, Jul 24, 2023, 3:34 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,24 +1,27 @@
 import re
 
 # TODO
-card = input("card: ")
+def main():
+    card = input("card: ")
+    pattern(card)
 
-def pattern():
+def pattern(card):
     amex = "^(34|37)\d{13}$"
     visa = "^(4)(\d{12}$|\d{15}$)"
     master = "^(51|55)\d{14}$"
     if re.match(master, card):
-        return "master"
+        print("MASTER")
     elif re.match(amex, card):
-        return "amex"
+        print("AMEX")
     elif re.match(visa, card):
-        return "visa"
+        print("VISA")
     else:
-        return "invalid"
+        print("INVALID")
 
-def check_sum():
-    
-
+# def check_sum():
 
 
-# amex:378282246310005, visa:4111111111111111, master: 5555555555554444
+
+if __name__ == "__main__":
+    main()
+# amex:4111111111111111, visa:4111111111111111, master: 5555555555554444

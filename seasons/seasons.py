05213ba55613138ff334068e5760d233ec412503
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-08-09 19:45:49 +0000

    Wed, Aug 9, 2023, 8:45 PM +01:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,11 +1,11 @@
 from datetime import date
 
-class date:
-    
+class Date:
+    def.__sub__(date.today(), birthday):
 
 def main():
-    birthday = input("Enter birthday: ")
-    print(operator.__sub__(date.today(), birthday))
+    # birthday = input("Enter birthday: ")
+    # print(operator.__sub__(date.today(), birthday))
 
 if __name__ == "__main__":
     main()

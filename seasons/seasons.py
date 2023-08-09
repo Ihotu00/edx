c6810c51811431202922129749221ac18551a0e5
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-08-09 19:34:01 +0000

    Wed, Aug 9, 2023, 8:34 PM +01:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -2,7 +2,7 @@ from datetime import date
 
 def main():
     birthday = input("Enter birthday: ")
-    print(__sub__(date.t))
+    print(__sub__(date.today(), birthday))
 
 if __name__ == "__main__":
     main()

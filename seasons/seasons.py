89b407a4c78d95950687a35dce813b3c0769158c
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-08-10 17:11:19 +0000

    Thu, Aug 10, 2023, 6:11 PM +01:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -8,7 +8,7 @@ def main():
     sec = input("Enter birthday: ")
     if re.fullmatch(r"(\d{4})-(\d{2})-(\d{2})", sec):
         min = int((date.today() - date.fromisoformat(sec)).total_seconds() / 60)
-        print(p.number_to_words(min).capitalize())
+        print(p.number_to_words(min, andword="").capitalize())
     else:
         sys.exit()
 

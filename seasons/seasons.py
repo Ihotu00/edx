2085bd7467806c6d4ab99baf594b4bae6640758f
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-08-10 16:33:10 +0000

    Thu, Aug 10, 2023, 5:33 PM +01:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,7 +1,10 @@
 from datetime import date, timedelta
+import re
+import sys
 
 def main():
     sec = input("Enter birthday: ")
+    if fullmatch()
     min = (date.today() - date.fromisoformat(sec)).total_seconds() / 60
     print(min)
 

b8ee21bb4c24ac581a1fc0baf0027b08afec8888
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-08-10 17:42:40 +0000

    Thu, Aug 10, 2023, 6:42 PM +01:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -2,6 +2,8 @@ from seasons import minutes
 import pytest
 
 def test_input():
-    assert minutes("2022-08-10") == "Five hundred twenty-five thousand, six hundred minutes"
     with pytest.raises(SystemExit):
         minutes("Feb 12 1999")
+
+def test_output():
+    assert minutes("2022-08-10") == "Five hundred twenty-five thousand, six hundred minutes"

9e61954e05fe434a74205db62a9a675a747c080f
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-08-10 16:42:10 +0000

    Thu, Aug 10, 2023, 5:42 PM +01:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -4,7 +4,7 @@ import sys
 
 def main():
     sec = input("Enter birthday: ")
-    if fullmatch(r"%d{4}-%d{2}-%d{2}", sec):
+    if re.fullmatch(r"\d{4}-\d{2}-\d{2}", sec):
         min = (date.today() - date.fromisoformat(sec)).total_seconds() / 60
         print(min)
     else:

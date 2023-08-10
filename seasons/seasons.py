37c12da29c27ad9809c9aceed20a22e45423bd38
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-08-10 16:36:06 +0000

    Thu, Aug 10, 2023, 5:36 PM +01:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -4,9 +4,11 @@ import sys
 
 def main():
     sec = input("Enter birthday: ")
-    if fullmatch()
-    min = (date.today() - date.fromisoformat(sec)).total_seconds() / 60
-    print(min)
+    if fullmatch(r"%d{4}-%d{2}-%d{2}", sec):
+        min = (date.today() - date.fromisoformat(sec)).total_seconds() / 60
+        print(min)
+    else:
+        sys.exit()
 
 if __name__ == "__main__":
     main()

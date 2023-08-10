c3870df940c656b14f1c185e03f8dc82d7977f1e
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-08-10 16:04:46 +0000

    Thu, Aug 10, 2023, 5:04 PM +01:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,15 +1,19 @@
-from datetime import date
+from datetime import date, timedelta, datetime
 
 class Date:
     def __init__(self, birthday):
         self.birthday = birthday
-    def __sub__(date.today(), self):
-        return date.today() - birthday
+    def __sub__(self):
+        return (date.today() - datetime.strptime(self.birthday)).total_seconds()
 
 def main():
-    Date(input("Enter birthday: "))
-    # birthday = input("Enter birthday: ")
-    # print(operator.__sub__(date.today(), birthday))
+    sec = Date(input("Enter birthday: "))
+    print(sec.__sub__())
+    # sec = input("Enter birthday: ")
+    # print((date.today() - sec).total_seconds())
 
 if __name__ == "__main__":
     main()
+
+
+    # 2000-04-15

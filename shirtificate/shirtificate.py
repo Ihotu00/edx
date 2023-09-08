dfd75b2c146186555985ae883e529367c12cc48a
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-09-08 11:41:52 +0000

    Fri, Sep 8, 2023, 12:41 PM +01:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -7,7 +7,7 @@ from fpdf import FPDF
 #         self.cell(30, 10, "CS50 Shirtificate", border=1, align="C")
 #         self.ln(20)
 
-pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf = FPDF()
 pdf.add_page()
 pdf.image("shirtificate.png", 0, 100, 180)
 pdf.output("shirtificate.pdf")

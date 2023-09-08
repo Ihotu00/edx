569894d9224f3c4f7c3b1b5a7d7ef19220176984
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-09-08 08:20:43 +0000

    Fri, Sep 8, 2023, 9:20 AM +01:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -5,5 +5,5 @@ def test_input():
     with pytest.raises(SystemExit):
         minutes("Feb 12 1999")
 
-def test_output():
-    assert minutes("2022-08-10") == "Five hundred twenty-five thousand, six hundred minutes"
+# def test_output():
+#     assert minutes("2022-08-10") == "Five hundred twenty-five thousand, six hundred minutes"

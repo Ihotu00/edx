2720ba0f9ce0fbde71db5a47d6a7a8e16ede81c6
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-09-08 08:45:48 +0000

    Fri, Sep 8, 2023, 9:45 AM +01:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,10 +1,10 @@
 class Jar:
-    def __init__(self, capacity=-12):
+    def __init__(self, capacity=12):
         if capacity < 1:
             raise ValueError("Must be 1-12")
 
 def main():
-    cap = input("Capacity: ")
+    cap = int(input("Capacity: "))
     Jar(cap)
 
 if __name__ == "__main__":

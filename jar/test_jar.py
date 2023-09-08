e79f7ba4600acdb8975e20e9b1102129c4cb261b
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-09-08 10:21:12 +0000

    Fri, Sep 8, 2023, 11:21 AM +01:00

--- jar/test_jar.py
+++ jar/test_jar.py
@@ -1,4 +1,19 @@
 from jar import Jar
+import pytest
 
 def test_init():
-    assert init(Jar(-10))
+    with pytest.raises(ValueError):
+        init(Jar(-10))
+
+def test_str():
+    jar = Jar()
+    assert str(jar) == ""
+    jar.deposit(1)
+    assert str(jar) == "🍪"
+    jar.deposit(7)
+    assert str(jar) == "🍪🍪🍪🍪🍪🍪🍪"
+
+def test_deposit():
+    jar = Jar()
+    jar.deposit(1)
+    assert 

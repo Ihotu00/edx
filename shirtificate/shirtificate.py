938c952401dd808e755787a42c2d601f14dfd880
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2023-09-08 11:04:20 +0000

    Fri, Sep 8, 2023, 12:04 PM +01:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -1,13 +1,13 @@
 from fpdf import FPDF
 
-class PDF(FPDF):
-    def header(self):
-        self.set_font("Courier", "B", 15)
-        self.cell(80)
-        self.cell(30, 10, "CS50 Shirtificate", border=1, align="C")
-        self.ln(20)
+# class PDF(FPDF):
+#     def header(self):
+#         self.set_font("Courier", "B", 15)
+#         self.cell(80)
+#         self.cell(30, 10, "CS50 Shirtificate", border=1, align="C")
+#         self.ln(20)
 
-pdf = PDF()
+pdf = FPDF()
 pdf.add_page()
 pdf.image("shirtificate.png")
 pdf.output("shirtificate.pdf")

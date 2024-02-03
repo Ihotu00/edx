abc44463aa4ae920b8bf54ba5bebbc9eb7e6e14b
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2024-02-03 23:46:14 +0000

    Sun, Feb 4, 2024, 12:46 AM +01:00

--- homepage/scripts.js
+++ homepage/scripts.js
@@ -2,4 +2,5 @@ function navigate(page) {
     var content = document.querySelector('#content');
     content.src = page;
 }
-
+var tableContent = [{first: "Ihotu", last: "Ifenne", handle: "@ii"},
+                    {first: "John", last: "Doe", handle:"@jd"}]

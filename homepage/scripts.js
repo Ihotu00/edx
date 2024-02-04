eeaff2325e10dd1bbc790c121474dc801a4639d9
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2024-02-04 00:46:56 +0000

    Sun, Feb 4, 2024, 1:46 AM +01:00

--- homepage/scripts.js
+++ homepage/scripts.js
@@ -6,24 +6,24 @@ var tableContent = [{first: "Ihotu", last: "Ifenne", handle: "@ii"},
                     {first: "John", last: "Doe", handle:"@jd"}]
 
 function addRow() {
-    console.log("started")
-    // const first = document.getElementById('#first').value;
-    // const last = document.getElementById('#last').value;
-    // const handle = document.getElementById('#handle').value;
-    // const table = document.getElementById('#table');
-    // console.log(first)
-    // var row = document.createElement('tr');
-    // var cellFirst = document.createElement('td');
-    // var textFirst = document.createTextNode(first);
-    // var cellLast = document.createElement('td');
-    // var textLast = document.createTextNode(last);
-    // var cellHandle = document.createElement('td');
-    // var textHandle = document.createTextNode(handle);
-    // cellFirst.appendChild(textFirst);
-    // row.appendChild(cellFirst);
-    // cellLast.appendChild(textLast);
-    // row.appendChild(cellLast);
-    // cellHandle.appendChild(textHandle);
-    // row.appendChild(cellHandle);
-    // table.appendChild(row);
+    // console.log("started")
+    const first = document.getElementById('#first').value;
+    const last = document.getElementById('#last').value;
+    const handle = document.getElementById('#handle').value;
+    const table = document.getElementById('#table');
+    console.log(first)
+    var row = document.createElement('tr');
+    var cellFirst = document.createElement('td');
+    var textFirst = document.createTextNode(first);
+    var cellLast = document.createElement('td');
+    var textLast = document.createTextNode(last);
+    var cellHandle = document.createElement('td');
+    var textHandle = document.createTextNode(handle);
+    cellFirst.appendChild(textFirst);
+    row.appendChild(cellFirst);
+    cellLast.appendChild(textLast);
+    row.appendChild(cellLast);
+    cellHandle.appendChild(textHandle);
+    row.appendChild(cellHandle);
+    table.appendChild(row);
 }

dc88f6ce6377d52481de50124e257163e9232c8b
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2024-02-04 01:12:42 +0000

    Sun, Feb 4, 2024, 2:12 AM +01:00

--- homepage/scripts.js
+++ homepage/scripts.js
@@ -2,8 +2,6 @@ function navigate(page) {
     var content = document.querySelector('#content');
     content.src = page;
 }
-var tableContent = [{first: "Ihotu", last: "Ifenne", handle: "@ii"},
-                    {first: "John", last: "Doe", handle:"@jd"}]
 
 function addRow() {
     const first = document.querySelector('#first').value;
@@ -17,18 +15,8 @@ function addRow() {
     cellFirst.innerHTML = first;
     cellLast.innerHTML = last;
     cellHandle.innerHTML = handle;
-    // var row = document.createElement('tr');
-    // var cellFirst = document.createElement('td');
-    // var textFirst = document.createTextNode(first);
-    // var cellLast = document.createElement('td');
-    // var textLast = document.createTextNode(last);
-    // var cellHandle = document.createElement('td');
-    // var textHandle = document.createTextNode(handle);
-    // cellFirst.appendChild(textFirst);
-    // row.appendChild(cellFirst);
-    // cellLast.appendChild(textLast);
-    // row.appendChild(cellLast);
-    // cellHandle.appendChild(textHandle);
-    // row.appendChild(cellHandle);
-    // table.appendChild(row);
+}
+
+function deleteRow() {
+    
 }

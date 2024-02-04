937dd8718ab2f5b160893170804a7ef1aa55cab5
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2024-02-04 00:18:52 +0000

    Sun, Feb 4, 2024, 1:18 AM +01:00

--- homepage/scripts.js
+++ homepage/scripts.js
@@ -6,5 +6,15 @@ var tableContent = [{first: "Ihotu", last: "Ifenne", handle: "@ii"},
                     {first: "John", last: "Doe", handle:"@jd"}]
 
 function addRow() {
-    var table = document.getElementById
+    const first = document.getElementById('first');
+    const last = document.getElementById('last');
+    const handle = document.getElementById('handle');
+    const table = document.getElementById('#table');
+    var row = document.createElement('tr');
+    var cell = document.createElement('td');
+    var text = document.createTextNode(first);
+    var cell = document.createElement('td');
+    var text = document.createTextNode(first);
+    var cell = document.createElement('td');
+    var text = document.createTextNode(first);
 }

2b749efc010280edc6d0f7ba68634b3e66237aac
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2024-04-11 23:45:40 +0000

    Fri, Apr 12, 2024, 12:45 AM +01:00

--- homepage/tic-tac-toe.js
+++ homepage/tic-tac-toe.js
@@ -29,10 +29,10 @@ function ticTacToe(div) {
 }
 
 function currentPlayerWins() {
-    console.log(board['1']); console.log(board['2']); console.log(board['3']);
+    // console.log(board['1']); console.log(board['2']); console.log(board['3']);
     // console.log(board);
-    // if (
-    //     (board['1'] == board['2'] && board['2'] == board['3'])
+    if (
+        (board['1'] == board['2'] && board['2'] == board['3'])
         // (board['4'] == board['5'] && board['5'] == board['6']) ||
         // (board['7'] == board['8'] && board['8'] == board['9']) ||
         // (board['1'] == board['4'] && board['4'] == board['7']) ||
@@ -40,5 +40,6 @@ function currentPlayerWins() {
         // (board['3'] == board['6'] && board['6'] == board['9']) ||
         // (board['1'] == board['5'] && board['5'] == board['9']) ||
         // (board['3'] == board['5'] && board['5'] == board['7'])
-        // ) { return true; }
+        ) { return true; }
+    else { return false; }
 }

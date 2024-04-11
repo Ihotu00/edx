240b8af1639a23cd4cecd64aa82aba74fc0f480c
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2024-04-11 20:55:26 +0000

    Thu, Apr 11, 2024, 9:55 PM +01:00

--- homepage/rock-paper-scissors.js
+++ homepage/rock-paper-scissors.js
@@ -6,12 +6,15 @@ let moves = {
 let turn = "player1";
 
 function play(move) {
+    var turnTtext = document.getElementById('turn-text').innerHTML;
     if (turn == "player1") {
         moves.player1 = move;
         turn = "player2";
+        turnText = "Second Player's Move";
     }
 
     if (turn == "player2") {
         moves.player2 = move;
+        turnText = "Click the submit button to see the winner";
     }
 }

e36e3171b8f235f9ed069b29f06c9a3b1608ea5e
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2024-04-11 22:45:23 +0000

    Thu, Apr 11, 2024, 11:45 PM +01:00

--- homepage/tic-tac-toe.js
+++ homepage/tic-tac-toe.js
@@ -1,6 +1,5 @@
-let moves = {
-    "player1": "",
-    "player2": ""
+let board = {
+    "1": "", "2": "", "3": "", "4": "", "5": "", "6": "", "7": "", "8": "", "9": "",
 }
 
 let playerX = true;
@@ -8,9 +7,11 @@ let playerX = true;
 function ticTacToe(div) {
     if (playerX) {
         document.getElementById(`${div}`).innerHTML = 'X';
+        board[div] = 'X';
     }
     else {
         document.getElementById(`${div}`).innerHTML = 'O';
+        board[div] = 'O';
     }
     document.getElementById(`${div}`).parentElement.classList.add('disable-div');
     playerX = !playerX;

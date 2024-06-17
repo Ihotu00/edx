6ddbd0e18c3ca491480fa4d551719460bb5f1f35
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2024-06-17 03:51:28 +0000

    Mon, Jun 17, 2024, 4:51 AM +01:00

--- finance/app.py
+++ finance/app.py
@@ -88,17 +88,17 @@ def buy():
             logging.warning(user_cash[0]["cash"])
             try:
                 db.execute("INSERT INTO users_shares (user_id, symbol, shares) VALUES(?,?,?)",
-                       session["user_id"], request.form.get("symbol").lower(), request.form.get("shares").lower())
+                       session["user_id"], request.form.get("symbol").upper(), request.form.get("shares"))
                 logging.warning("Inserted first time")
             except(ValueError):
                 logging.warning("Already exist updating")
                 shares = db.execute("SELECT shares FROM users_shares WHERE user_id = ? AND symbol = ?",
-                                           session["user_id"], request.form.get("symbol").lower())
+                                           session["user_id"], request.form.get("symbol").upper())
                 shares[0]["shares"] += int(request.form.get("shares"))
                 db.execute("UPDATE users_shares SET shares = ?, last_modified_date = ? WHERE user_id = ? AND symbol = ?",
-                                         shares[0]["shares"], datetime.datetime.now(), session["user_id"], request.form.get("symbol").lower())
+                                         shares[0]["shares"], datetime.datetime.now(), session["user_id"], request.form.get("symbol").upper())
             db.execute("INSERT INTO users_transaction_history (user_id, symbol, shares, action) VALUES(?,?,?,?)",
-                       session["user_id"], request.form.get("symbol").lower(), request.form.get("shares"), "buy")
+                       session["user_id"], request.form.get("symbol").upper(), request.form.get("shares"), "buy")
             db.execute("UPDATE users SET cash = ? WHERE id = ?", user_cash[0]["cash"], session["user_id"])
             return redirect("/")
 
@@ -118,7 +118,7 @@ def login():
     # Forget any user_id
     session.clear()
 
-    # User reached route via POST (as by submitting a form via POST) 
+    # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
         # Ensure username was submitted
         if not request.form.get("username"):
@@ -245,7 +245,7 @@ def sell():
             return apology("must provide symbol")
 
         shares = db.execute("SELECT * FROM users_shares WHERE user_id = ? AND symbol = ?",
-                            session["user_id"], request.form.get("symbol").lower())
+                            session["user_id"], request.form.get("symbol").upper())
 
         cash = db.execute("SELECT * FROM users WHERE id =", session["user_id"])
 
@@ -258,9 +258,9 @@ def sell():
         cash[0]["cash"] += price
 
         db.execute("UPDATE users_shares SET shares = ?, last_modified_date = ? WHERE user_id = ? AND symbol = ?",
-                                         shares[0]["shares"], datetime.datetime.now(), session["user_id"], request.form.get("symbol").lower())
+                                         shares[0]["shares"], datetime.datetime.now(), session["user_id"], request.form.get("symbol").upper())
         db.execute("INSERT INTO users_transaction_history (user_id, symbol, shares, action) VALUES(?,?,?,?)",
-                    session["user_id"], request.form.get("symbol").lower(), request.form.get("shares"), "sell")
+                    session["user_id"], request.form.get("symbol").upper(), request.form.get("shares"), "sell")
         db.execute("UPDATE users SET cash = ? WHERE id = ?", cash[0]["cash"], session["user_id"])
         return redirect("/")
 

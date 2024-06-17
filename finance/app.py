38b9e37b47acba2ecca0b557607d709f63b05837
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2024-06-17 05:08:49 +0000

    Mon, Jun 17, 2024, 6:08 AM +01:00

--- finance/app.py
+++ finance/app.py
@@ -282,3 +282,36 @@ def sell():
         db.execute("UPDATE users SET cash = ? WHERE id = ?", cash[0]["cash"], session["user_id"])
         return redirect("/")
 
+
+@app.route("/change-password", methods=["GET", "POST"])
+def changePassword():
+    """Change Password"""
+
+    if request.method == "GET":
+        return render_template("change-password.html")
+
+    else:
+        # Ensure username was submitted
+        if not request.form.get("old_password"):
+            return apology("must provide password", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("new_password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")

12fb64751200b189bbbe512aa4b1bcac6147d3e1
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2024-06-20 17:00:24 +0000

    Thu, Jun 20, 2024, 6:00 PM +01:00

--- cs50x/project/app.py
+++ cs50x/project/app.py
@@ -71,7 +71,7 @@ def post():
 
     if request.form.get("message"):
         db.execute("INSERT INTO blog_posts(user_id, post, group_id) VALUES(?,?,?)", session["user_id"], request.form.get("message"), group_id)
-    return 'Message Sent' #redirect("/")
+    return redirect("/", tab=)
 
 
 @app.route("/create/group", methods=["POST"])

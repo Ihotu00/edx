cd59bb490bddb00b41e33010179f9c1483e83f93
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2024-06-21 15:39:16 +0000

    Fri, Jun 21, 2024, 4:39 PM +01:00

--- cs50x/project/app.py
+++ cs50x/project/app.py
@@ -83,13 +83,11 @@ def create_group():
         try:
             db.execute("INSERT INTO groups(created_by, group_name) VALUES(?,?)", session["user_id"], request.form.get("group_name"))
         except (ValueError):
-            flash("Sorry that name is unavailbale. Try something else")
-            return redirect("/")
+            return "Sorry that name is unavailbale. Try something else", 400
 
         group = db.execute("SELECT * FROM groups WHERE group_name = ?", request.form.get("group_name"))
         db.execute("INSERT INTO users_groups(user_id, group_id) VALUES(?,?)", session["user_id"], group[0]["id"])
-        flash("Created Successfully")
-    return redirect("/")
+        return "Created Successfully", 200
 
 
 

b07f29b3ea7c16353eeabc6cf7b5a24784736762
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2024-06-25 12:42:09 +0000

    Tue, Jun 25, 2024, 1:42 PM +01:00

--- cs50x/project/app.py
+++ cs50x/project/app.py
@@ -56,7 +56,7 @@ def index(client, client_name):
 
     if client == "user":
         posts = db.execute("""SELECT post, blog_posts.id AS id, blog_posts.creation_time, group_name AS name, photo FROM blog_posts
-                           INNER JOIN users on users.id = blog_posts.user_id WHERE username = ? OR group_id IN (?) ORDER BY blog_posts.creation_time DESC""", client_name, [group["id"] for group in groups])
+                           INNER JOIN groups on groups.id = blog_posts.group_id WHERE blog_posts.user_id = ? OR group_id IN (?) ORDER BY blog_posts.creation_time DESC""", client_name, [group["id"] for group in groups])
 
     if client == "group":
         posts = db.execute("""SELECT post, blog_posts.id AS id, blog_posts.creation_time, group_name AS name photo FROM blog_posts

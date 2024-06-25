edad36b642927f5c5568acad19c28bd0c8298e5e
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2024-06-25 16:01:47 +0000

    Tue, Jun 25, 2024, 5:01 PM +01:00

--- cs50x/project/static/scripts.js
+++ cs50x/project/static/scripts.js
@@ -6,8 +6,7 @@ var is_canvas = false;
 document.addEventListener('DOMContentLoaded', function() {
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
-    // console.log(name);
-    console.log(JSON.stringify(name));
+    console.log(name);
 })
 
 function hide(id) {

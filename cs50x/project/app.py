d3fade49121a002d05d594d5fa0b2b630c5052a9
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2024-06-26 12:12:14 +0000

    Wed, Jun 26, 2024, 1:12 PM +01:00

--- cs50x/project/app.py
+++ cs50x/project/app.py
@@ -63,6 +63,7 @@ def index(client, client_name):
 
     if client == "group":
         header = db.execute("SELECT groupname AS name, photo FROM groups WHERE groupname = ?", client_name)
+        logging.warning(session["user_groups"])
         if header[0]["name"] in [group["groupname"] for group in session["user_groups"]]:
             header[0]["is_member"] = "true"
         else:

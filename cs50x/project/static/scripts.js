eb45ef92a659455b0ae93293f57a9a70272fb2ee
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2024-06-27 10:58:16 +0000

    Thu, Jun 27, 2024, 11:58 AM +01:00

--- cs50x/project/static/scripts.js
+++ cs50x/project/static/scripts.js
@@ -109,26 +109,26 @@ function get_group_msg(id, name) {
 }
 
 function create_group() {
-    data = {
+    group = {
         'group_name': document.getElementById('group_name').value,
         'group_photo': document.getElementById('group_photo').src,
         'access': document.getElementById('public-group').checked ? "public" : "private",
     }
     console.log(data)
-    // $.ajax({
-    //     url: '/create/group',
-    //     type: 'POST',
-    //     contentType: 'application/json',
-    //     data: JSON.stringify({ 'group_name': document.getElementById('group_name').value, 'group_photo': document.getElementById('group_photo').src }),
-    //     success: function(response) {
-    //         console.log(response)
-    //         location.pathname = response;
-    //     },
-    //     error: function(error) {
-    //         console.log(error.responseText);
-    //         document.getElementById("group-taken").innerHTML = error.responseText;
-    //     }
-    // });
+    $.ajax({
+        url: '/create/group',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(group),
+        success: function(response) {
+            console.log(response)
+            location.pathname = response;
+        },
+        error: function(error) {
+            console.log(error.responseText);
+            document.getElementById("group-taken").innerHTML = error.responseText;
+        }
+    });
 }
 
 function login(event, url) {

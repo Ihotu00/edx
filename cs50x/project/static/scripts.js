064e97ef8e60d4cf7fb9e8f451403b4e29a350a2
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2024-06-27 11:23:06 +0000

    Thu, Jun 27, 2024, 12:23 PM +01:00

--- cs50x/project/static/scripts.js
+++ cs50x/project/static/scripts.js
@@ -22,6 +22,29 @@ function show_modal(id) {
     _modal.show();
 }
 
+// create alerts
+function create_alert(parentId) {
+    const alertPlaceholder = document.getElementById(parentId)
+    const appendAlert = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
+
+    alertPlaceholder.append(wrapper)
+    }
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Nice, you triggered this alert message!', 'success')
+  })
+}
+
 function send_post(event) {
     event.preventDefault();
     show("spinner")

5594edd54a919202ee54fd7e93ac7ec8c5d37928
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2024-06-27 15:23:18 +0000

    Thu, Jun 27, 2024, 4:23 PM +01:00

--- cs50x/project/static/scripts.js
+++ cs50x/project/static/scripts.js
@@ -30,8 +30,7 @@ function create_alert(parentId, message, type, icon) {
     const wrapper = document.createElement('div')
     wrapper.innerHTML = [
         `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-        `   <span class="material-icons-outlined">${icon}</span>`,
-        `   <div>${message}</div>`,
+        `   <div><span class="material-symbols-outlined">${icon}</span> ${message}</div>`,
         '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
         '</div>'
     ].join('')

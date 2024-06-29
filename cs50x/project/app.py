5f8c260f3a4cbcdec362fbafd501522a968d0f7d
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2024-06-29 14:47:53 +0000

    Sat, Jun 29, 2024, 3:47 PM +01:00

--- cs50x/project/app.py
+++ cs50x/project/app.py
@@ -107,8 +107,13 @@ def post(name, type):
                     session["user_name"], data["post_body"], data["group_name"], data["type"])
 
         if data["group_name"] != None:
-            post = db.execute("""SELECT group_name, photo, user_name, post,  FROM blog_posts
+            post = db.execute("""SELECT group_name, photo, user_name, post, blog_posts.creation_time FROM blog_posts
                               INNER JOIN groups on groupname = group_name WHERE id = (SELECT DISTINCT last_insert_rowid())""")
+
+        else:
+            post = db.execute("""SELECT photo, user_name, post, blog_posts.creation_time FROM blog_posts
+                              INNER JOIN users on username = user_name WHERE id = (SELECT DISTINCT last_insert_rowid())""")
+
         post = db.execute("SELECT * FROM blog_posts WHERE id = (SELECT DISTINCT last_insert_rowid())")
 
         if type == "comment":

4c50d16f3ba014d4d0d3c1e7952be76fcdddfd7e
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2024-07-01 15:25:15 +0000

    Mon, Jul 1, 2024, 4:25 PM +01:00

--- cs50x/project/static/scripts.js
+++ cs50x/project/static/scripts.js
@@ -58,7 +58,8 @@ function create_post() {
         }),
         success: function(response) {
             console.log(response)
-            location.pathname = encodeURIComponent(`/post?${response}`);
+            console.log(encodeURIComponent(`/post?${response}`))
+            // location.pathname = encodeURIComponent(`/post?${response}`);
         },
         error: function(error) {
             console.log(error);

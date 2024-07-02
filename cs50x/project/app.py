aac88e3db65c5b9991ae94ad9f9d6f50d86e13fc
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2024-07-02 16:23:16 +0000

    Tue, Jul 2, 2024, 5:23 PM +01:00

--- cs50x/project/app.py
+++ cs50x/project/app.py
@@ -69,9 +69,9 @@ def index(client, client_name):
         # if client_name != session["user_name"]:
         header = db.execute("SELECT username AS name, photo FROM users WHERE username = ?", client_name)
         posts = db.execute("""SELECT post, blog_posts.id AS id, blog_posts.creation_time, group_name, user_name, photo FROM blog_posts
-                            INNER JOIN groups on groupname = group_name WHERE user_name = ? OR group_name IN (?) AND type != 'comment'
+                            INNER JOIN groups on groupname = group_name WHERE (user_name = ? OR group_name IN (?)) AND type != 'comment'
                             UNION SELECT post, blog_posts.id AS id, blog_posts.creation_time, group_name, user_name AS name, photo FROM blog_posts
-                            INNER JOIN users on username = user_name WHERE user_name = ? and group_name is null AND type != 'comment' ORDER BY blog_posts.creation_time DESC""", client_name, [group["groupname"] for group in session["user_groups"]], client_name)
+                            INNER JOIN users on username = user_name WHERE (user_name = ? and group_name is null) AND type != 'comment' ORDER BY blog_posts.creation_time DESC""", client_name, [group["groupname"] for group in session["user_groups"]], client_name)
 
     if client == "group":
         feed = client_name

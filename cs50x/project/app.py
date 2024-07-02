b1ddc44c40f439c8de564286231a3bc32ce4faaf
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2024-07-02 10:13:34 +0000

    Tue, Jul 2, 2024, 11:13 AM +01:00

--- cs50x/project/app.py
+++ cs50x/project/app.py
@@ -146,8 +146,8 @@ def post(type):
                 comments = db.execute("SELECT * FROM blog_posts INNER JOIN comments ON post_id = id WHERE id = ?", request.args.get('id'))
 
                 logging.warning(comments)
-                for comment in comments:
-                    comment["photo"] = db.execute("SELECT photo FROM users WHERE username = ?", comment["user_name"])
+                # for comment in comments:
+                #     comment["photo"] = db.execute("SELECT photo FROM users WHERE username = ?", comment["user_name"])
 
                 return render_template("post.html", post=post[0], comments=comments)
 

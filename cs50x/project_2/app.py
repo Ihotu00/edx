c75de68f6aad742ca5bc3abcd816763e29339018
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2024-07-07 23:01:32 +0000

    Mon, Jul 8, 2024, 12:01 AM +01:00

--- cs50x/project_2/app.py
+++ cs50x/project_2/app.py
@@ -152,7 +152,7 @@ def post():
             post = db.execute("""SELECT posts.id, posts.created_by, post, posts.creation_time AS creation_time, photo,
                           (SELECT sum(vote) FROM votes WHERE post_id = posts.id) AS votes,
                           (SELECT COUNT(*) FROM comments WHERE post_id = posts.id) AS comments_count FROM posts
-                          INNER JOIN comments on post_id = 
+                          INNER JOIN comments on comment_id = ?
                           INNER JOIN users on username = created_by WHERE posts.id = post_id""")
 
 

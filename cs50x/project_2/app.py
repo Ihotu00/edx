fae83de9ba23705111c307e2426056c37e7384e0
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2024-07-07 14:31:10 +0000

    Sun, Jul 7, 2024, 3:31 PM +01:00

---
+++ cs50x/project_2/app.py
@@ -0,0 +1,223 @@
+import os
+import logging
+from datetime import datetime, timedelta
+import re
+from uuid import uuid4
+
+from cs50 import SQL
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
+from functools import wraps
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+# Configure application
+app = Flask(__name__)
+
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///blog.db")
+
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/latest/patterns/viewdecorators/
+    """
+
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+@app.template_filter('date')
+def format_date(date):
+    datetime_object = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+    return datetime_object.strftime("%b %d, %Y")
+
+@app.template_filter('time')
+def format_time(date):
+    datetime_object = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+    return datetime_object.strftime("%H:%M")
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/feed/<client>/<client_name>")
+@login_required
+def index(client, client_name):
+    return render_template("index.html")
+
+
+
+@app.route("/post")
+@app.route("/post/submit", methods=["POST"])
+@login_required
+def post():
+    if request.method == "POST":
+
+            if request.get_json():
+                data = request.get_json()
+
+            else:
+                return "Failed to get input.", 400
+
+            if not data["post_body"]:
+                return "Please fill out the post body", 400
+            if not data["type"]:
+                return "Couldn't parse type", 400
+
+            id = f"{uuid4()}"
+            db.execute("BEGIN")
+            db.execute("INSERT INTO posts(id, created_by, post, type) VALUES(?,?,?,?)",
+                        id, session["user_name"], data["post_body"], data["type"])
+
+            if data["type"] == "comment_post":
+                if not request.args.get('id'):
+                    return "Could not parse request", 400
+
+                og_post = db.execute("SELECT * FROM posts WHERE id = ?", request.args.get('id'))
+                if not og_post:
+                    return "Could not find post", 400
+
+                db.execute("INSERT INTO comments(post_id, comment_id) VALUES(?,?)", request.args.get('id'), id)
+                db.execute("COMMIT")
+                return f"{request.args.get('id')}", 200
+
+            else:
+                db.execute("COMMIT")
+                return f"{id}", 200
+
+    else:
+        post = db.execute("""SELECT posts.id, posts.created_by, post, posts.creation_time AS creation_time, photo,
+                          (SELECT COUNT(*) FROM comments WHERE post_id = posts.id) AS comments_count FROM posts
+                          INNER JOIN users on username = created_by WHERE posts.id = ?""", request.args.get('id'))
+
+        if not post:
+            return "Could not find post", 400
+
+        comments = db.execute("SELECT *, (SELECT COUNT(*)) AS count FROM posts INNER JOIN comments ON comment_id = id WHERE post_id = ?", request.args.get('id'))
+
+        for comment in comments:
+            comment["photo"] = (db.execute("SELECT photo FROM users WHERE username = ?", comment["created_by"]))[0]["photo"]
+        return render_template("post.html", post=post[0], comments=comments)
+
+
+@app.route("/", methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    session.clear()
+
+    if request.method == "POST":
+        if request.get_json():
+            data = request.get_json()
+
+            if not data["username"]:
+                return "Please provide Username", 400
+
+            elif not data["password"]:
+                return "Please provide Password", 400
+
+            rows = db.execute(
+                "SELECT * FROM users WHERE username = ?",  data["username"]
+            )
+
+            if len(rows) != 1 or not check_password_hash(
+                rows[0]["password"],  data["password"]
+            ):
+                return "Invalid username/password", 400
+
+            session["user_id"] = rows[0]["id"]
+            session["user_photo"] = rows[0]["photo"]
+            session["user_name"] = rows[0]["username"]
+            session["user_groups"] = db.execute(
+                "SELECT groupname, photo FROM groups INNER JOIN users_groups on groups.groupname = users_groups.group_name WHERE users_groups.user_name = ? ORDER BY users_groups.creation_time DESC", session["user_name"])
+
+            return "Login Successful", 200
+
+        else:
+            return "invalid json", 400
+
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    session.clear()
+
+    return redirect("/login")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+
+    session.clear()
+
+    if request.method == "POST":
+
+        if request.get_json():
+            data = request.get_json()
+
+            if not data["username"]:
+                return "Please provide Username", 400
+
+            elif not data["password"]:
+                return "Please provide Password", 400
+
+            elif not data["confirmation"]:
+                return "Please confirm Password", 400
+
+            elif data["password"] != data["confirmation"]:
+                return "Passwords do not match", 400
+
+            elif not data["photo"]:
+                return "Photo is null", 400
+
+            password_hash = generate_password_hash(data["password"])
+            id = f"{uuid4()}"
+
+            try:
+                db.execute(
+                    "INSERT INTO users (id, username, password, photo) VALUES(?,?,?, ?)",
+                    id,
+                    data["username"],
+                    password_hash,
+                    data["photo"]
+                )
+            except ValueError:
+                return "Username taken", 400
+
+            rows = db.execute(
+                "SELECT * FROM users WHERE username = ?", data["username"]
+            )
+
+            session["user_id"] = rows[0]["id"]
+            session["user_photo"] = rows[0]["photo"]
+            session["user_name"] = rows[0]["username"]
+
+            return "Registration Successful", 200
+
+    else:
+        return render_template("register.html")
---
+++ cs50x/project_2/static/scripts.js
@@ -0,0 +1,185 @@
+var _modal;
+var _canvas;
+var is_canvas = false;
+
+// initialize tootips
+document.addEventListener('DOMContentLoaded', function() {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+})
+
+function hide(id) {
+    document.getElementById(id).classList.add('hide');
+}
+
+
+function show(id) {
+    document.getElementById(id).classList.remove('hide');
+}
+
+function show_modal(id) {
+    _modal = new bootstrap.Modal(document.getElementById(id))
+    _modal.show();
+}
+
+// create alerts
+function create_alert(parentId, message, type, icon) {
+    icon = !icon ? 'notifications' : icon
+    console.log(icon)
+    const alertPlaceholder = document.getElementById(parentId)
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div class="d-flex"><span class="material-symbols-outlined me-1">${icon}</span> ${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
+
+    alertPlaceholder.append(wrapper)
+}
+
+function create_post(event, type, id) {
+    console.log(event)
+    event.preventDefault();
+    show(event.target.loader_button.id)
+    hide(event.target.submit_button.id)
+    $.ajax({
+        url: id != null ? `/post/submit?id=${id}` : `/post/submit`,
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({
+            'post_body': event.target.post_body.value,
+            'type': type
+        }),
+        success: function(response) {
+            location.pathname = `/post`;
+            location.search = `?id=${response}`
+        },
+        error: function(error) {
+            console.log(error);
+            hide(event.target.loader_button.id)
+            show(event.target.submit_button.id)
+            create_alert(event.target.parentNode.querySelector("[name='response']").id, `${error.responseText}`, 'danger', 'error')
+        }
+    });
+}
+
+function get_group_msg(id, name) {
+    document.getElementById('group_id').value = id;
+    header = document.getElementById("group_posts")
+    header.replaceChildren();
+    header.insertAdjacentHTML('beforeend', `<h1 id="group_header" class="sticky-top bg-dark">${name}</h1>`)
+    $.ajax({
+        url: '/post?group_id=' + id,
+        type: 'GET',
+        success: function(response) {
+            for (x = 0; x < response.length; x++) {
+                header.insertAdjacentHTML('beforeend',
+                    `<div class="card mt-5 text-bg-primary">
+                        <div class="card-header text-start">
+                            <img src=${response[x]["photo"]} style="clip-path: circle()" width="20" height="20">
+                            ${response[x]["username"]}
+                        </div>
+                        <div class="card-body">
+                            ${response[x]["post"]}
+                        </div>
+                        <div class="card-footer text-end">
+                            ${response[x]["creation_time"]}
+                        </div>
+                    </div>`);
+            }
+        },
+        error: function(error) {
+            if (error.status == 404) {
+                console.log(error)
+                header.insertAdjacentHTML('beforeend',
+                    `<div class="center" id="no_posts_msg">
+                        <h1 class="text-white-50" style="font-size: 15cqb; color: white">${error.responseText}</h1>
+                    </div>`)
+            }
+        }
+    });
+    if (is_canvas) close_canvas();
+}
+
+function create_group() {
+    show('create-group-loader')
+    hide('submit-new-group')
+    group = {
+        'group_name': document.getElementById('group_name').value,
+        'group_photo': document.getElementById('group_photo').src != "" ? document.getElementById('group_photo').src : null,
+        'access': document.getElementById('public-group').checked ? "public" : "private",
+    }
+    console.log(group)
+    $.ajax({
+        url: '/create/group',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(group),
+        success: function(response) {
+            console.log(response)
+            location.pathname = response;
+        },
+        error: function(error) {
+            hide('create-group-loader')
+            show('submit-new-group')
+            console.log(error.responseText);
+            create_alert('create-group-response', `${error.responseText}`, 'danger', 'error')
+        }
+    });
+}
+
+function login(event, url) {
+    event.preventDefault();
+    show("spinner")
+    document.getElementById("submit-form").classList.add("disabled")
+    $.ajax({
+        url: url,
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({
+            'username': event.target.username.value,
+            'password': event.target.password.value,
+            'confirmation': url == "/register" ? event.target.confirmation.value : "",
+            'photo': url == "/register" ? event.target.photo.src : ""
+        }),
+        success: function(response) {
+            location.pathname = `/feed/user/${event.target.username.value}`;
+        },
+        error: function(error) {
+            console.log(error)
+            create_alert("login-response", `${error.responseText}`, 'danger', 'error')
+            hide("spinner")
+            document.getElementById("submit-form").classList.remove("disabled")
+        }
+    });
+}
+
+function imageUploaded(event, id) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file)
+    reader.onloadend = () => {
+        document.getElementById(id).src = reader.result
+    };
+}
+
+function show_canvas() {
+    document.getElementById('close-canvas').classList.remove('hide');
+    document.getElementById('group-parent-div').classList.remove('d-none');
+    document.getElementById('group-parent-div').classList.add('offcanvas');
+    document.getElementById('group-parent-div').classList.add('offcanvas-start');
+    document.getElementById('group-parent-div').style['max-height'] = '100%';
+    _canvas = new bootstrap.Offcanvas('#group-parent-div');
+    _canvas.show();
+    is_canvas = true;
+}
+
+function close_canvas() {
+    _canvas.hide();
+    document.getElementById('close-canvas').classList.add('hide');
+    document.getElementById('group-parent-div').classList.add('d-none');
+    document.getElementById('group-parent-div').classList.remove('offcanvas');
+    document.getElementById('group-parent-div').classList.remove('offcanvas-start');
+    document.getElementById('group-parent-div').style['max-height'] = '300px';
+}

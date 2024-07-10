6b7588f773306b11c8c99b50235671c6d43924ad
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2024-07-10 22:47:03 +0000

    Wed, Jul 10, 2024, 11:47 PM +01:00

--- cs50x/project_2/app.py
+++ cs50x/project_2/app.py
@@ -96,7 +96,7 @@ def profile(username):
         ORDER BY users_followers.creation_time DESC""", session["user_name"])
     votes = None
     if username == session["user_name"]:
-        votes = db.execute("""SELECT post, posts.id AS id, posts.creation_time, created_by, photo, title, type FROM posts
+        votes = db.execute("""SELECT post, posts.id AS id, posts.creation_time, created_by, photo, title, type, vote FROM posts
                        INNER JOIN votes on post_id = posts.id INNER JOIN users on users.username = created_by
                        WHERE votes.username = ?""", username)
     header = {"name": username, "photo": posts[0]["photo"]}

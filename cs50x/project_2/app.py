4dd8cba81ade8c9ed6afb60f4c0b4c86910711ee
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2024-07-28 15:15:56 +0000

    Sun, Jul 28, 2024, 4:15 PM +01:00

--- cs50x/project_2/app.py
+++ cs50x/project_2/app.py
@@ -80,7 +80,7 @@ def home():
 
     posts = db.execute("""SELECT post, posts.id AS id, posts.creation_time, created_by, photo, title,
                        (SELECT IFNULL(SUM(vote), 0) FROM votes WHERE post_id = posts.id) AS votes FROM posts
-                        INNER JOIN users on username = created_by WHERE type != 'comment_post' LIMIT = 3 ORDER BY votes DESC""")
+                        INNER JOIN users on username = created_by WHERE type != 'comment_post' ORDER BY votes DESC LIMIT 3""")
 
     totalUsers = db.execute("SELECT SUM(id) FROM users")
     totalPosts = db.execute("SELECT SUM(id) FROM posts WHERE type != 'comment_post'")

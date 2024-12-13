2079d9a532136b3f6b30356177f175d129260496
Ihotu00 <136355127+Ihotu00@users.noreply.github.com>
2024-12-13 13:17:45 +0000

    Fri, Dec 13, 2024, 2:17 PM +01:00

--- cs50p/project/tic-tac-toe.py
+++ cs50p/project/tic-tac-toe.py
@@ -1,42 +1,11 @@
-import pygame
+import pyglet
 
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
-dt = 0
+window = pyglet.window.Window()
+image = pyglet.resource.image('kitten.jpg')
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-player_speed = 300  # movement speed
+@window.event
+def on_draw():
+    window.clear()
+    image.blit(0, 0)
 
-while running:
-    # poll for events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
-
-    # Draw the player circle
-    pygame.draw.circle(screen, "red", player_pos, 40)
-
-    # Get keys pressed and update position
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= player_speed * dt
-    if keys[pygame.K_s]:
-        player_pos.y += player_speed * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= player_speed * dt
-    if keys[pygame.K_d]:
-        player_pos.x += player_speed * dt
-
-    # flip() the display to put your work on screen
-    pygame.display.flip()
-
-    # calculate delta time (dt) to make the movement frame-rate independent
-    dt = clock.tick(60) / 1000  # 60 FPS limit
-
-pygame.quit()
+pyglet.app.run()
